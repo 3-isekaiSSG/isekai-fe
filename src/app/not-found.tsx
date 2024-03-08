76271840ab7db04240884854e7b43d2e3e979832
heeyoung <9hiyoung@gmail.com>
2024-03-08 14:44:20 +0900

    Feat: 이전 페이지로 버튼 > Link로 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image'
-import Button from '@/components/not-found/Button'
+import Link from 'next/link'
 
 export default function NotFound() {
   return (
     <div className="w-screen h-screen relative">
       <div className="absolute -translate-x-2/4 -translate-y-2/4 text-center w-full px-[30px] py-0 left-2/4 top-2/4">
-        <Image alt="not-found" src="/images/404.png" width={140} height={100} />
-        <h1 className="text-2xl leading-6 font-bold mt-[30px]">
+        <div className="max-w-[140px] mx-auto my-0">
+          <Image
+            alt="not-found"
+            src="/images/404.png"
+            width={200}
+            height={100}
+          />
+        </div>
+        <h1 className="leading-6 font-bold mt-[30px]">
           원하셨던
           <br />
           페이지가 아닌가요?
@@ -18,7 +25,13 @@ export default function NotFound() {
           <br />
           이전 페이지에서 다시 한번 시도해 주세요.
         </p>
-        <Button />
+        {/* FIXME: 이전 페이지로 돌아가기 router.back 하면 버튼으로 해야하지 않나? */}
+        <Link
+          href="/"
+          className="block bg-[#222] text-white text-[15px] leading-[52px] no-underline max-w-[315px] mt-[30px] mb-0 mx-auto rounded-lg"
+        >
+          이전페이지로 돌아가기
+        </Link>
       </div>
     </div>
   )
