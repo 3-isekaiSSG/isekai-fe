3c447759da4172c027f1c2b2e4c9e3c05c9e0923
구희영 <9hiyoung@gmail.com>
2024-03-11 23:50:12 +0900

    Feat: TabBar 퍼블리싱

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
 
 export default function RootLayout({
   children,
@@ -9,6 +10,7 @@ export default function RootLayout({
     <>
       <Header />
       {children}
+      <TabBar />
     </>
   )
 }
