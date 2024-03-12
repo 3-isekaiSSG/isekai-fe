bace3caf36519caa7ba5c6731ff4cb98bd5d9299
heeyoung <9hiyoung@gmail.com>
2024-03-12 15:21:13 +0900

    Feat: 카테고리 테마추천

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,3 +1,18 @@
+import CategoryList from '@/components/category/CategoryList'
+import ThemeList from '@/components/category/ThemeList'
+
 export default function Page() {
-  return <div>카테고리</div>
+  return (
+    <>
+      <div>
+        <CategoryList />
+      </div>
+      <div className="relative mb-[20px]">
+        <h2 className="pb-[5px] pt-[20px] pl-[15px] text-[14px] font-bold text-[color:var(--m-colors-black)]">
+          테마추천
+        </h2>
+        <ThemeList />
+      </div>
+    </>
+  )
 }
---
+++ src/components/category/CategoryList.tsx
@@ -0,0 +1,3 @@
+export default function CategoryList() {
+  return <div>리스트트트ㅡㅌ</div>
+}
