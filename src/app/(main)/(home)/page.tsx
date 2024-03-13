a2423caeb7e823d369c348db7877b6c1dc9fc811
heeyoung <9hiyoung@gmail.com>
2024-03-13 20:21:31 +0900

    Feat: 카드 할인 퍼블리싱

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Banner from '@/components/home/Banner'
 import EventCarousel from '@/components/home/EventCarousel'
 import ItemSlider from '@/components/home/ItemSlider'
+import CardSale from '@/components/home/CardSale'
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <div className="h-[40px]" />
       {/* 유니버스 클럽 배너 */}
       <Banner />
+      <div className="h-[40px]" />
+      <CardSale />
     </main>
   )
 }
