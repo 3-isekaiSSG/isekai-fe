03204b497dce55ac042ff4170bb9e60690a37057
wnsvy7203 <wnsvy7203@gmail.com>
2024-03-15 17:35:07 +0900

    rename: member layout / fix: join.module.css

--- src/components/JoinEmail/MemberInfo.tsx
+++ src/components/JoinEmail/MemberInfo.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import style from '@/components/join.module.css'
+import CheckCert from './CheckCert'
 
 export default function MemberInfo() {
   const EmailId = 'Hi'
@@ -153,62 +154,8 @@ export default function MemberInfo() {
                     인증번호 발송
                   </button>
                 </div>
-                {/* 인증번호 발송 버튼 눌러야 나오도록 구현 - 컴포넌트 따로 뗄까 흠.. */}
-                <div className={`${style.cmem_inpbtn_set} ${style.otpNoWrap}`}>
-                  <span className={style.cmem_inp_txt}>
-                    <input
-                      id="optNo"
-                      name="optNo"
-                      type="text"
-                      title="인증번호"
-                      placeholder="인증번호 입력"
-                    />
-                  </span>
-                  <button
-                    type="button"
-                    id="resend"
-                    className={`${style.cmem_btn} ${style.cmem_btn_gray3}`}
-                    // onClick="reqOtp(); return false;"
-                    // href="javascript:void(0);"
-                  >
-                    재발송
-                  </button>
-                </div>
-                <span
-                  className={`${style.cmem_noti} ${style.otpNoWrap}`}
-                  // style="display: none"
-                >
-                  <em id="remainTime" className={style.auth_code_noti}>
-                    남은시간<span>3</span>분<span>00</span>초
-                  </em>
-                </span>
-                <div
-                  className={`${style.cmem_btn_area} ${style.otpNoWrap}`}
-                  // style="display: none"
-                >
-                  <ul>
-                    <li>
-                      <button
-                        type="button"
-                        className={`${style.cmem_btn} ${style.cmem_btn_blkline}`}
-                        // onclick="initOtp(); return false;"
-                        // href="javascript:void(0);"
-                      >
-                        <span className={style.notranslate}>취소</span>
-                      </button>
-                    </li>
-                    <li>
-                      <button
-                        type="button"
-                        className={`${style.cmem_btn} ${style.cmem_btn_gray}`}
-                        // onclick="otpCert.chkOtp(); return false;"
-                        // href="javascript:void(0);"
-                      >
-                        <span className={style.notranslate}>확인</span>
-                      </button>
-                    </li>
-                  </ul>
-                </div>
+                {/* 인증번호 발송 버튼 눌러야 나오도록 구현 */}
+                <CheckCert />
               </dd>
             </dl>
           </div>
