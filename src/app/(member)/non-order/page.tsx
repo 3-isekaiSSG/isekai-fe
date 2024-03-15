4a375f4bfa7999277b652efd3b3490c0cad2a050
wnsvy7203 <wnsvy7203@gmail.com>
2024-03-16 00:10:41 +0900

    Design: non-order

--- src/app/(member)/non-order/page.tsx
+++ src/app/(member)/non-order/page.tsx
@@ -1,5 +1,20 @@
-import NonOrder from '@/components/NonOrder'
+import Link from 'next/link'
+import style from '@/components/login.module.css'
+import NonForm from '@/components/forms/NonForm'
 
 export default function Page() {
-  return <NonOrder />
+  return (
+    <div id="m_content" className={style.cmem_ct_nomem}>
+      <NonForm />
+      <div className={style.cmem_login_btm}>
+        <span className={style.cmem_btm_txt}>
+          SSG.COM 회원가입을 하시면 더 많은 혜택을 받으실 수 있습니다.
+        </span>
+        <Link href="/join-intro" className={style.cmem_btm_btn}>
+          <span>회원가입</span>
+          <span className={style.cmpop_ico_arr} />
+        </Link>
+      </div>
+    </div>
+  )
 }
