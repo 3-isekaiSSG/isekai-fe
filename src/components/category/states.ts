07d0d34027b98d852329726ad9d8a4861666649d
heeyoung <9hiyoung@gmail.com>
2024-03-20 16:09:46 +0900

    Feat: category API 연결

--- src/components/category/states.ts
+++ src/components/category/states.ts
@@ -6,12 +6,14 @@ interface ThemeItem {
 }
 
 export interface CategoryMType {
+  id: number
   colored: boolean
   categoryMId: number
   mediumName: string
 }
 
 export interface CategoryLMType {
+  id: number
   categoryMList: CategoryMType[]
   categoryLId: number
   largeName: string
