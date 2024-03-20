201fb96d57865d0a863e4743d82b87095d3d2656
heeyoung <9hiyoung@gmail.com>
2024-03-20 09:37:21 +0900

    Feat: API 요청 작성중"

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,10 +1,21 @@
 import CategoryList from '@/components/category/CategoryList'
 import ThemeList from '@/components/category/ThemeList'
 
-export default function Page() {
+async function getCategoryLM() {
+  // const res = (
+  //   await fetch(`${process.env.NEXT_PUBLIC_API}/products/category`)
+  // ).json()
+  // console.log(res)
+  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+}
+
+export default async function Page() {
   // TODO: 카테고리 리스트를 fetch 받아 CategoryList에 Prop 시키기
   // const categoryLM = []
   const data = undefined
+  await getCategoryLM()
 
   return (
     <>
