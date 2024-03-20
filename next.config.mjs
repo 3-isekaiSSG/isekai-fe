1a8b782d687069db08add81a9af640dfe555962d
heeyoung <9hiyoung@gmail.com>
2024-03-20 14:07:51 +0900

    Feat: category API 연결

--- next.config.mjs
+++ next.config.mjs
@@ -36,6 +36,14 @@ const nextConfig = {
       },
     ]
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API}/:path*`,
+      },
+    ]
+  },
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -2,20 +2,19 @@ import CategoryList from '@/components/category/CategoryList'
 import ThemeList from '@/components/category/ThemeList'
 
 async function getCategoryLM() {
-  // const res = (
-  //   await fetch(`${process.env.NEXT_PUBLIC_API}/products/category`)
-  // ).json()
-  // console.log(res)
-  await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then((response) => response.json())
-    .then((json) => console.log(json))
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API}/products/category`,
+    )
+    const data = await response.json()
+    return data
+  } catch (err) {
+    return []
+  }
 }
 
 export default async function Page() {
-  // TODO: 카테고리 리스트를 fetch 받아 CategoryList에 Prop 시키기
-  // const categoryLM = []
-  const data = undefined
-  await getCategoryLM()
+  const data = await getCategoryLM()
 
   return (
     <>
