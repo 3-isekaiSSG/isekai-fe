6ac4d71d43f00ea0027e743c474393ef6338ba80
heeyoung <9hiyoung@gmail.com>
2024-03-22 19:35:08 +0900

    Style: 최근 검색어 없음

--- src/containers/search/RecentSearch.tsx
+++ src/containers/search/RecentSearch.tsx
@@ -12,65 +12,69 @@ export default function RecentSearch() {
   // })
   // localStorage.getItem('currentSearch')
 
-  const searchList = [
-    'dddddddddddddddddd',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'ㅇ',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-    'dtd',
-  ]
+  // const searchList = [
+  //   'dddddddddddddddddd',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'ㅇ',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  //   'dtd',
+  // ]
+  const searchList: [] = []
 
   /** 최근 검색어 삭제 */
   const handleDelete = () => {}
+
+  /** 최근 검색어 전체 삭제 */
+  const handleClear = () => {}
 
   return (
     <section className="mt-5">
@@ -78,39 +82,50 @@ export default function RecentSearch() {
         <h3 className="text-sm text-[color:var(--m-colors-gray900)] font-bold">
           최근 검색어
         </h3>
-        <button
-          type="button"
-          className="text-xs font-normal h-auto min-h-[auto] text-[color:var(--m-colors-gray600)] min-w-[2.5rem] m-0 p-0"
-        >
-          전체삭제
-        </button>
+        {searchList.length !== 0 && (
+          <button
+            onClick={handleClear}
+            type="button"
+            className="text-xs font-normal h-auto min-h-[auto] text-[color:var(--m-colors-gray600)] min-w-[2.5rem] m-0 p-0"
+          >
+            전체삭제
+          </button>
+        )}
       </div>
 
       <div className="flex-row flex items-center justify-start my-2.5  pe-4 overflow-x-auto flex-nowrap">
-        {searchList.map((item, idx) => (
-          <div
-            // eslint-disable-next-line react/no-array-index-key
-            key={idx}
-            className={`w-fit text-[color:var(--m-colors-gray500)] ${styles.searchItem}`}
-          >
-            <span className="inline-flex items-center text-[color:var(--m-colors-gray800)] bg-[color:var(--m-colors-white)] shadow-[rgb(229,229,229)_0px_0px_0px_1px_inset] font-normal text-[13px] h-9 rounded-full ps-[12px] pe-[12px]">
-              <Link
-                href={`/search/${item}`}
-                className="flex items-center h-full py-3 text-inherit"
-              >
-                {item}
-              </Link>
-              <button
-                aria-label="삭제"
-                type="button"
-                className="w-5 h-5 leading-[1em] text-[color:var(--m-colors-gray600)] flex items-center justify-center"
-                onClick={handleDelete}
-              >
-                <CgClose />
-              </button>
-            </span>
+        {searchList.length !== 0 ? (
+          searchList.map((item, idx) => (
+            <div
+              // eslint-disable-next-line react/no-array-index-key
+              key={idx}
+              className={`w-fit text-[color:var(--m-colors-gray500)] ${styles.searchItem}`}
+            >
+              <span className="inline-flex items-center text-[color:var(--m-colors-gray800)] bg-[color:var(--m-colors-white)] shadow-[rgb(229,229,229)_0px_0px_0px_1px_inset] font-normal text-[13px] h-9 rounded-full ps-[12px] pe-[12px]">
+                <Link
+                  href={`/search/${item}`}
+                  className="flex items-center h-full py-3 text-inherit"
+                >
+                  {item}
+                </Link>
+                <button
+                  aria-label="삭제"
+                  type="button"
+                  className="w-5 h-5 leading-[1em] text-[color:var(--m-colors-gray600)] flex items-center justify-center"
+                  onClick={handleDelete}
+                >
+                  <CgClose />
+                </button>
+              </span>
+            </div>
+          ))
+        ) : (
+          <div className="flex items-center justify-start my-2.5">
+            <p className="text-[13px] text-[color:var(--m-colors-gray600)]">
+              최근 검색어 내역이 없습니다.
+            </p>
           </div>
-        ))}
+        )}
       </div>
     </section>
   )
