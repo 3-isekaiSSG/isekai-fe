f4ed6c8ce3508ebd5ee042c6ea2b9cac26f7fb9a
wnsvy7203 <wnsvy7203@gmail.com>
2024-04-09 20:30:41 +0900

    Feat: redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,22 @@
-// eslint-disable-next-line no-restricted-exports
-export { default } from 'next-auth/middleware'
+import { NextRequest, NextResponse } from 'next/server'
+
+export default async function middleware(request: NextRequest) {
+  const sessionRequest = new Request(
+    `${request.nextUrl.origin}/api/auth/session`,
+    {
+      headers: request.headers,
+    },
+  )
+  const sessionResponse = await fetch(sessionRequest)
+  const session = await sessionResponse.json()
+
+  if (!session.user) {
+    return NextResponse.redirect(
+      new URL('/login?callbackUrl=myssg', request.url),
+    )
+  }
+
+  return NextResponse.next()
+}
 
 export const config = { matcher: ['/myssg'] }
